d4498f634e769d4c697b608f24b6289e1b3b07c3
ravindugm <ravindumiyuranga@gmail.com>
2021-11-07 10:02:48 +0530

    Initial Commit

---
+++ src/main/java/com/rmdev/rmexpensetracker/RmExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rmdev.rmexpensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmExpenseTrackerApplication.class, args);
+	}
+
+}
