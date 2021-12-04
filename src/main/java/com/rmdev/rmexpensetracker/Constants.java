c0754872f9487a979f1d31bf1b4d542ff60f1a4f
ravindum <ravindu@hsenidoutsourcing.com>
2021-12-04 12:08:41 +0530

    Add JWT token generator

---
+++ src/main/java/com/rmdev/rmexpensetracker/Constants.java
@@ -0,0 +1,8 @@
+package com.rmdev.rmexpensetracker;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "Zr4u7wzCFJaNdRgUkXp2s5v8yA";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
