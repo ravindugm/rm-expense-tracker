3cddcf623e0cca2dc7fab1878dd11a94b087d53e
ravindugm <ravindumiyuranga@gmail.com>
2021-12-25 13:34:07 +0530

    Add create transactions feature

---
+++ src/main/java/com/rmdev/rmexpensetracker/repository/TransactionRepository.java
@@ -0,0 +1,20 @@
+package com.rmdev.rmexpensetracker.repository;
+
+import com.rmdev.rmexpensetracker.entity.Transaction;
+import com.rmdev.rmexpensetracker.exception.RmBadRequestException;
+import com.rmdev.rmexpensetracker.exception.RmResourceNotFoundException;
+
+import java.util.List;
+
+public interface TransactionRepository {
+
+    List<Transaction> findAll(Integer userId, Integer categoryId);
+
+    Transaction findById(Integer userId, Integer categoryId, Integer transactionId) throws RmResourceNotFoundException;
+
+    Integer create(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate) throws RmBadRequestException;
+
+    void update(Integer userId, Integer categoryId, Integer transactionId, Double amount, String note, Long transactionDate) throws RmBadRequestException;
+
+    void removeById(Integer userId, Integer categoryId, Integer transactionId) throws RmResourceNotFoundException;
+}
